a5bb2c4c8c2b88f00564ecc52bd82fb5aa874c2e
AaronC714 <aaronc0714@gmail.com>
2025-03-27 23:25:10 +0800

    pseudocode added

--- batch_6/create_program_that_do_the_same_functionality_without_using_endswith_function.py
+++ batch_6/create_program_that_do_the_same_functionality_without_using_endswith_function.py
@@ -1,1 +1,4 @@
 #Prog05. Create a program that do the same functionality without using endswith() function.
+#process of determining if input and prameter are the same
+#user inputs (statment and wanted parameters)
+#print result, true or false 
