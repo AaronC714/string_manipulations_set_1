7bddf5d4afe1ec2c8f28aaa8ede5c1738d493e12
AaronC714 <aaronc0714@gmail.com>
2025-03-27 21:13:31 +0800

    define lower case function of user input added

--- batch_6/program_that_do_the_same_functionality_without_using_lower_function.py
+++ batch_6/program_that_do_the_same_functionality_without_using_lower_function.py
@@ -4,3 +4,14 @@
 #print user input in lower case
 
 user_input = str(input("Enter a text here: "))
+
+def lower_case(user_input):
+    result = " "
+
+    for char in user_input:
+        if 'A' <= char <= 'Z':
+            result += chr(ord(char) + 32)
+        else:
+            result += char
+
+    return result
