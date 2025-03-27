323aeb193acd0782ea4c4c35fa6d51d8525eacac
AaronC714 <aaronc0714@gmail.com>
2025-03-27 22:40:45 +0800

    pseudocode added

--- batch_6/program_that_do_the_same_functionality_without_using_isupper_function.py
+++ batch_6/program_that_do_the_same_functionality_without_using_isupper_function.py
@@ -1,1 +1,4 @@
 #Create a program that do the same functionality without using isupper() function.
+#ask user for input
+#identify if text is uppercased without using isupper()
+#print correct boolean
