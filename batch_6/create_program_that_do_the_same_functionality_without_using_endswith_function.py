de2986e7aa4448b945c3da2526b98c3517a81f17
AaronC714 <aaronc0714@gmail.com>
2025-03-28 00:04:10 +0800

    define endswith added

--- batch_6/create_program_that_do_the_same_functionality_without_using_endswith_function.py
+++ batch_6/create_program_that_do_the_same_functionality_without_using_endswith_function.py
@@ -1,4 +1,7 @@
 #Prog05. Create a program that do the same functionality without using endswith() function.
 #process of determining if input and prameter are the same
 #user inputs (statment and wanted parameters)
-#print result, true or false 
+#print result, true or false
+
+def ends_with(text, suffix):
+    return text[-len(suffix):] == suffix if len(suffix) <= len(text) else False
