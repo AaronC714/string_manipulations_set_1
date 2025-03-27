fcda64556c496c703d1b00695718fb7be5ddc515
AaronC714 <aaronc0714@gmail.com>
2025-03-27 17:51:38 +0800

    pseudocode added

--- batch_6/program_that_do_same_functionality_without_using_lstrip_function.py
+++ batch_6/program_that_do_same_functionality_without_using_lstrip_function.py
@@ -1,1 +1,4 @@
 #Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
+#ask user for full name with spaces before the name
+#process result without using lstrip()
+#print result
