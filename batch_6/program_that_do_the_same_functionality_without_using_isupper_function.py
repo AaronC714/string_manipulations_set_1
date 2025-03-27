f93c02007837546ced884c09d4dfb444a3edfe7e
AaronC714 <aaronc0714@gmail.com>
2025-03-27 23:09:30 +0800

    define is upper case with upper count and lower count added

--- batch_6/program_that_do_the_same_functionality_without_using_isupper_function.py
+++ batch_6/program_that_do_the_same_functionality_without_using_isupper_function.py
@@ -1,4 +1,9 @@
-#Create a program that do the same functionality without using isupper() function.
+#Prog04. Create a program that do the same functionality without using isupper() function.
+#create process to identify if text is uppercased without using isupper()
 #ask user for input
-#identify if text is uppercased without using isupper()
 #print correct boolean
+
+
+def is_upper_case(user_input):
+    upper_count = 0
+    lower_count = 0
