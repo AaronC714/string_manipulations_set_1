57bfa20643fad870bde17f5cf39192912c9e5b92
AaronC714 <aaronc0714@gmail.com>
2025-03-27 19:58:47 +0800

    process to get removed prefix

--- batch_6/program_without_using_removeprefix_function.py
+++ batch_6/program_without_using_removeprefix_function.py
@@ -2,3 +2,8 @@
 #user input name
 #process in removing prefix under desired parameter
 #print user name after process
+
+def remove_prefix(text, prefix):
+    if text.startswith(prefix):  #If the string starts with the given prefix
+        return text[len(prefix):]  #Remove prefix by slicing
+    return text  # Return the original string if prefix is not found
