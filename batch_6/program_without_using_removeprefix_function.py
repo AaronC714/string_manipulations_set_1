cd0aaa4b98a618402533fa42563142d2253075c7
AaronC714 <aaronc0714@gmail.com>
2025-03-27 19:56:06 +0800

    pseudocode added

--- batch_6/program_without_using_removeprefix_function.py
+++ batch_6/program_without_using_removeprefix_function.py
@@ -1,1 +1,4 @@
-#
+#Prog02. Create a program that do the same functionality without using removeprefix() function.
+#user input name
+#process in removing prefix under desired parameter
+#print user name after process
