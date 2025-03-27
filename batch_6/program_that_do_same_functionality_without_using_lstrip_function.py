4ea36da3bd4752915421b34fdc450aca7f78c942
AaronC714 <aaronc0714@gmail.com>
2025-03-27 18:07:56 +0800

    removing leading spaces process added

--- batch_6/program_that_do_same_functionality_without_using_lstrip_function.py
+++ batch_6/program_that_do_same_functionality_without_using_lstrip_function.py
@@ -3,4 +3,10 @@
 #process result without using lstrip()
 #print result
 
-user_name = int(input("Enter your name with multiple spaces before it (ex.     Juan Dela Cruz): "))
+user_name = input("Enter your name with multiple spaces before it (ex.     Juan Dela Cruz): ")
+
+def remove_leading_spaces(user_name):
+    index = 0
+    while index < len(user_name) and user_name[index] == ' ':
+        index += 1
+    return user_name[index:]
