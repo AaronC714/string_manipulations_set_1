f8a40a014ef4ea64965db5c95aac75f8c661f956
AaronC714 <aaronc0714@gmail.com>
2025-03-27 20:01:34 +0800

    user input and their chosen parameters added

--- batch_6/program_without_using_removeprefix_function.py
+++ batch_6/program_without_using_removeprefix_function.py
@@ -7,3 +7,6 @@ def remove_prefix(text, prefix):
     if text.startswith(prefix):  #If the string starts with the given prefix
         return text[len(prefix):]  #Remove prefix by slicing
     return text  # Return the original string if prefix is not found
+
+user_name = str(input("Enter your name: "))
+parameters = str(input("Enter prefix to remove: "))
