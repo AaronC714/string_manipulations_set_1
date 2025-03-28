e7ba8a38a4f6d7d84c61f63efe11ff19250b2e45
AaronC714 <aaronc0714@gmail.com>
2025-03-28 12:37:23 +0800

    pseudocode added

--- batch_6/create_program_that_do_the_same_functionality_without_using_capitalize_function.py
+++ batch_6/create_program_that_do_the_same_functionality_without_using_capitalize_function.py
@@ -1,1 +1,4 @@
 #Prog09. Create a program that do the same functionality without using capitalize() function.
+#create def process for similar capitalize function
+#ask for user input
+#print result in capitalized form
