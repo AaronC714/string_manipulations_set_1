f7d1bef325ccd06927d976f112f22ad7025c5edc
AaronC714 <aaronc0714@gmail.com>
2025-03-28 12:51:45 +0800

    def process including upper and lower function added

--- batch_6/create_program_that_do_the_same_functionality_without_using_capitalize_function.py
+++ batch_6/create_program_that_do_the_same_functionality_without_using_capitalize_function.py
@@ -2,3 +2,10 @@
 #create def process for similar capitalize function
 #ask for user input
 #print result in capitalized form
+
+def mimic_capitalize(text):
+    if len(text) > 0:
+        # Capitalize the first letter and make the rest lowercase
+        return text[0].upper() + text[1:].lower()
+    else:
+        return text
