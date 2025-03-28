21e58cd4a7d9e173659f495c3e4a2cab50bbb95b
AaronC714 <aaronc0714@gmail.com>
2025-03-28 10:17:28 +0800

    def left just process added

--- batch_6/create_program_that_do_the_same_functionality_without_using_ljust_function.py
+++ batch_6/create_program_that_do_the_same_functionality_without_using_ljust_function.py
@@ -2,3 +2,9 @@
 #create def for same functionality as ljust function
 #ask for user's input
 #print result
+
+def left_just(text, num_spaces):
+    if not num_spaces.isdigit():
+        return "Invalid input, must be an integer"
+    
+    return text + "_" * int(num_spaces)
