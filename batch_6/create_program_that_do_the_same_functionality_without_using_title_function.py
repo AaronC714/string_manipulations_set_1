fb42e013bb335a25ec70b0f120c462d3134af5fa
AaronC714 <aaronc0714@gmail.com>
2025-03-28 13:12:27 +0800

    def process added for title case function

--- batch_6/create_program_that_do_the_same_functionality_without_using_title_function.py
+++ batch_6/create_program_that_do_the_same_functionality_without_using_title_function.py
@@ -2,3 +2,14 @@
 #create def process for title function
 #ask for user input
 #print result
+
+def title_case(text):
+    words = text.split()
+    result = []
+
+    for word in words:
+        # Capitalize the first letter of the word and make the rest lowercase
+        result.append(word[0].upper() + word[1:].lower())
+
+    # Join the words back together with spaces
+    return ' '.join(result)
