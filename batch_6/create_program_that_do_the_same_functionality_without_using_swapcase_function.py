d2f4e91531a306c06176d0acf00500637fdbf827
AaronC714 <aaronc0714@gmail.com>
2025-03-28 12:17:53 +0800

    pseudocode added

--- batch_6/create_program_that_do_the_same_functionality_without_using_swapcase_function.py
+++ batch_6/create_program_that_do_the_same_functionality_without_using_swapcase_function.py
@@ -1,1 +1,4 @@
 #Prog08. Create a program that do the same functionality without using swapcase() function.
+#create def process similar to swapcase function
+#ask for user input
+#print result in swapped case
