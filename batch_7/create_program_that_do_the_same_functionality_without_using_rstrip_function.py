269f16b79ac3e93fee413b8e6f33c95f2bcbb113
AaronC714 <aaronc0714@gmail.com>
2025-03-30 16:10:13 +0800

    def process for removing latter spaces added

--- batch_7/create_program_that_do_the_same_functionality_without_using_rstrip_function.py
+++ batch_7/create_program_that_do_the_same_functionality_without_using_rstrip_function.py
@@ -2,3 +2,9 @@
 #create def process for rstrip
 #ask for user input
 #print result
+
+def remove_latter_spaces(user_name):
+    index = -1 
+    while index >= -len(user_name) and user_name[index] == ' ':
+        index -= 1
+    return user_name[:index + 1]
