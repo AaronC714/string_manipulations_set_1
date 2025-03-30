7bc251039a22196db56bb99814b8972bf7e3568d
AaronC714 <aaronc0714@gmail.com>
2025-03-30 16:18:02 +0800

    pseudocode added

--- batch_7/create_program_that_do_the_same_functionality_without_using_removesuffix_function.py
+++ batch_7/create_program_that_do_the_same_functionality_without_using_removesuffix_function.py
@@ -1,1 +1,4 @@
-#
+#Prog02.Create a program that do the same functionality without using removesuffix() function.
+#create def process similar to removesuffix function
+#ask for user input
+#print result
