0a5b6cd93e59af26792cd08da21e529d7f15cb47
AaronC714 <aaronc0714@gmail.com>
2025-03-31 16:05:14 +0800

    pseudocode added

--- batch_7/create_program_that_do_the_same_functionality_without_using_startswith_function.py
+++ batch_7/create_program_that_do_the_same_functionality_without_using_startswith_function.py
@@ -1,1 +1,4 @@
 #Prog05.  Create a program that do the same functionality without using startswith() function.
+#create def process for startswith function
+#ask for user inputs
+#print final results
