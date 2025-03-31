5ea457dd186fa445cca3ad1c2242c32f39300c17
AaronC714 <aaronc0714@gmail.com>
2025-03-31 16:17:31 +0800

    simplify and print result

--- batch_7/create_program_that_do_the_same_functionality_without_using_startswith_function.py
+++ batch_7/create_program_that_do_the_same_functionality_without_using_startswith_function.py
@@ -8,3 +8,6 @@ def starts_with(text, prefix):
 
 user_input = input("Enter a string: ")
 start_choice = input("Prefix to check: ")
+
+final_prefix = starts_with(user_input, start_choice)
+print (final_prefix)
