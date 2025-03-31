e8118ec9c9ff262c2da89ff83eb40322e34f5c59
AaronC714 <aaronc0714@gmail.com>
2025-03-31 17:46:59 +0800

    def process added

--- batch_7/create_program_that_do_the_same_functionality_without_using_count_function.py
+++ batch_7/create_program_that_do_the_same_functionality_without_using_count_function.py
@@ -2,3 +2,12 @@
 #create def process for count function
 #ask user input
 #print result
+
+def manual_count(text, target):
+    count = 0
+    
+    for i in range(len(text) - len(target) + 1):
+        if text[i:i+len(target)] == target:
+            count += 1
+
+    return count
