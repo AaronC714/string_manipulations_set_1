bc4b7476fbc8da0c2e881e8c95ee44a398efd712
AaronC714 <aaronc0714@gmail.com>
2025-03-31 17:08:24 +0800

    def process added

--- batch_7/create_program_that_do_the_same_functionality_without_using_zfill_function.py
+++ batch_7/create_program_that_do_the_same_functionality_without_using_zfill_function.py
@@ -2,3 +2,14 @@
 #create def process for zfill function
 #ask user input
 #print result
+
+def zero_fill(number, total_width):
+    if not total_width.isdigit():
+        return "Invalid input, must be an integer"
+    
+    total_width = int(total_width)
+    
+    if total_width <= len(number):
+        return number
+    
+    return "0" * (total_width - len(number)) + number
