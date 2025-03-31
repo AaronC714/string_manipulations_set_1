889aca37a781ec1e9b32e71afe2c637108611718
AaronC714 <aaronc0714@gmail.com>
2025-03-31 16:43:15 +0800

    def process for rjust

--- batch_7/create_program_do_the_same_functionality_without_using_rjust_function.py
+++ batch_7/create_program_do_the_same_functionality_without_using_rjust_function.py
@@ -2,3 +2,14 @@
 #create def process for rjust function
 #ask user input
 #print result
+
+def right_just(text, total_width):
+    if not total_width.isdigit():
+        return "Invalid input, must be an integer"
+    
+    total_width = int(total_width)
+
+    if total_width <= len(text):
+        return text
+    
+    return "_" * (total_width - len(text)) + text
