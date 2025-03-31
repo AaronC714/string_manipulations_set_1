a1be7eba4ed33b29a2b672c8fc9db4913440f09f
AaronC714 <aaronc0714@gmail.com>
2025-03-31 13:31:01 +0800

    def process for upper case function

--- batch_7/create_program_that_do_the_same_functionality_without_using_upper_function.py
+++ batch_7/create_program_that_do_the_same_functionality_without_using_upper_function.py
@@ -1,1 +1,14 @@
 #Prog03. Create a program that do the same functionality without using upper() function.
+#create def process for upper function
+#ask user input
+#print result
+
+def upper_case(text):
+    result = " "
+
+    for char in text:
+        if 'a' <= char <= 'z':
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    return result
