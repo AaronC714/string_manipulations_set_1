15ff72619644b8807bfc92741264c19b3d42a2f5
AaronC714 <aaronc0714@gmail.com>
2025-03-31 20:21:32 +0800

    def process for rindex

--- batch_7/create_program_that_do_the_same_functionality_without_using_rindex_function.py
+++ batch_7/create_program_that_do_the_same_functionality_without_using_rindex_function.py
@@ -2,3 +2,9 @@
 #create def process for rindex function
 #ask for user input
 #print result
+
+def custom_rindex(string, substring):
+    for i in range(len(string) - len(substring), -1, -1):
+        if string[i:i + len(substring)] == substring:
+            return i
+    raise ValueError(f"'{substring}' not found in the given string.")
